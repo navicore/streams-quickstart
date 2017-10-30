0acf737db58e0b4115350cc5ccd47bab79482349
Ed Sweeney <ed@onextent.com>
2017-10-30 08:08:15 -0700

    scratch

--- src/main/scala/onextent/akka/quckstart/FactorialsExample.scala
+++ src/main/scala/onextent/akka/quckstart/FactorialsExample.scala
@@ -4,9 +4,10 @@ import java.nio.file.Paths
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, IOResult}
+import akka.stream._
 import akka.stream.scaladsl._
 import akka.util.ByteString
+import scala.concurrent.duration._
 
 import scala.concurrent.Future
 
@@ -19,8 +20,21 @@ object FactorialsExample extends App {
 
   val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
 
-  val result: Future[IOResult] =
-    factorials
-      .map(num => ByteString(s"$num\n"))
-      .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+  def lineSink(filename: String): Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(s => ByteString(s + "\n"))
+      .toMat(FileIO.toPath(Paths.get(filename)))(Keep.right)
+
+  // val result: Future[IOResult] =
+  //   factorials
+  //     .map(num => ByteString(s"$num\n"))
+  //     .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+
+  //factorials.map(_.toString).runWith(lineSink("factorial2.txt"))
+
+  factorials
+    .zipWith(Source(0 to 100))((num, idx) => s"$idx! = $num")
+    .throttle(1, 1.second, 1, ThrottleMode.shaping)
+    .runForeach(println)
+
 }
---
+++ src/main/scala/onextent/akka/quckstart/Tweets.scala
@@ -0,0 +1,21 @@
+package onextent.akka.quckstart
+
+final case class Author(handle: String)
+
+final case class Hashtag(name: String)
+
+final case class Tweet(author: Author, timestamp: Long, body: String) {
+  def hashtags: Set[Hashtag] =
+    body
+      .split(" ")
+      .collect {
+        case t if t.startsWith("#") => Hashtag(t.replaceAll("[^#\\w]", ""))
+      }
+      .toSet
+}
+
+class Tweets {
+
+  val akkaTag = Hashtag("#akka")
+
+}
